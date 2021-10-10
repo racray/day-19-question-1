e2dfeb90773dc8e5d1139aea772e5c00ba25f7cf
Rayed Khan <60972839+racray@users.noreply.github.com>
2021-10-10 15:17:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const list = document.querySelector(".pagination");
+list.innerHTML = `<li class="page-item">
+<span class="page-link">Previous</span>
+</li><li class="page-item"><a class="page-link" href="#" tabindex="-1">1</a></li>
+<li class="page-item"><a class="page-link" href="#">2</a></li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>    
+<li class="page-item"><a class="page-link" href="#">Next</a></li>`;
